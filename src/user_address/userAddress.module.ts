a13f29e756956d6b6f75be7e60ade7b85b88d62f
arziqulovsamandar <124333211+arziqulovsamandar@users.noreply.github.com>
2024-01-19 17:55:48 +0500

    useraddres table

---
+++ src/user_address/userAddress.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { UserAddressService } from './userAddress.service';
+import { UserAddressController } from './userAddress.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import {  UserAddress } from './models/userAddress.model';
+
+@Module({
+  imports: [SequelizeModule.forFeature([UserAddress])],
+  controllers: [UserAddressController],
+  providers: [UserAddressService],
+  exports: [UserAddressService],
+})
+export class UserAddressModule {}
