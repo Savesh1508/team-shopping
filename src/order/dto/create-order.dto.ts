840546aa99483aa1d9f7f0092e9143f2f78d7dae
arziqulovsamandar <124333211+arziqulovsamandar@users.noreply.github.com>
2024-01-22 14:33:21 +0500

    order table

--- src/order/dto/create-order.dto.ts
+++ src/order/dto/create-order.dto.ts
@@ -1,59 +1,64 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString ,IsEmail, IsDate, IsNumber, IsBoolean} from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  IsEmail,
+  IsDate,
+  IsNumber,
+  IsBoolean,
+  IsPhoneNumber,
+} from 'class-validator';
 
 export class CreateOrderDto {
   @ApiProperty({
-    example: 'Comment name',
-    description: 'Comment name',
+    example: 'Order name',
+    description: 'Order name',
   })
   @IsNotEmpty()
-  @IsString()
-  name: string;
-
-  @ApiProperty({
-    example: 'comment@gmail.com',
-    description: 'Comment email',
-  })
-  @IsEmail()
-  email: string;
+  @IsPhoneNumber()
+  phone: string;
 
   @ApiProperty({
     example: '12.12.2024',
-    description: 'Comment created_at',
+    description: 'Order date',
+  })
+  @IsNotEmpty()
+  @IsDate()
+  order_date: Date;
+
+  @ApiProperty({
+    example: '12.12.2024',
+    description: 'Order created_at',
   })
   @IsNotEmpty()
   @IsDate()
   created_at: Date;
 
   @ApiProperty({
-    example: "zo'r",
-    description: 'Comment text',
-  })
-  @IsNotEmpty()
-  @IsString()
-  text: string;
-
-  @ApiProperty({
-    example: "12234",
-    description: 'Comment rating',
-  })
-  @IsNotEmpty()
-  @IsNumber()
-  rating: number;
-
-  @ApiProperty({
-    example: "true",
-    description: 'Comment is_active',
+    example: 'true',
+    description: 'Order status',
   })
   @IsNotEmpty()
   @IsBoolean()
-  is_active: boolean;
+  status: boolean;
 
- 
-  @ApiProperty({ example: '1', description: 'Product id' })
-  product_id: number;
+  @ApiProperty({
+    example: '12234',
+    description: 'Comment rating',
+  })
+  @IsNotEmpty()
+  @IsString()
+  payment_type: string;
 
-  
+  @ApiProperty({ example: '1', description: 'userAddres id' })
+  userAddres_id: number;
+
   @ApiProperty({ example: '1', description: 'User id' })
   user_id: number;
+
+  @ApiProperty({ example: '1', description: ' basket id' })
+  basket_id: number;
+
+  @ApiProperty({ example: '1', description: 'cupon_code id' })
+  cupon_code_id: number;
 }
